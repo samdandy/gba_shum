cf10339da166590e30ac19d8ed6c8c29f4dad531
Sam dandy <sam.dandy@tigers.com>
2026-02-22 09:04:37 -0600

    adding bg

--- games/shum_shooter/build/bn_regular_bg_items_bg.h
+++ games/shum_shooter/build/bn_regular_bg_items_bg.h
@@ -9,11 +9,11 @@
 //
 //	bg_bn_gfx, 256x256@4, 
 //	+ palette 16 entries, not compressed
-//	+ 641 tiles (t|f|p reduced) not compressed
+//	+ 3 tiles (t|f|p reduced) not compressed
 //	+ regular map (flat), not compressed, 32x32 
-//	Total size: 32 + 20512 + 2048 = 22592
+//	Total size: 32 + 96 + 2048 = 2176
 //
-//	Time-stamp: 2026-02-07, 21:23:50
+//	Time-stamp: 2026-02-22, 08:59:57
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -22,8 +22,8 @@
 #ifndef GRIT_BG_BN_GFX_H
 #define GRIT_BG_BN_GFX_H
 
-#define bg_bn_gfxTilesLen 20512
-extern const bn::tile bg_bn_gfxTiles[641];
+#define bg_bn_gfxTilesLen 96
+extern const bn::tile bg_bn_gfxTiles[3];
 
 #define bg_bn_gfxMapLen 2048
 extern const bn::regular_bg_map_cell bg_bn_gfxMap[1024];
@@ -38,7 +38,7 @@ extern const bn::color bg_bn_gfxPal[16];
 namespace bn::regular_bg_items
 {
     constexpr inline regular_bg_item bg(
-            regular_bg_tiles_item(span<const tile>(bg_bn_gfxTiles, 641), bpp_mode::BPP_4, compression_type::NONE), 
+            regular_bg_tiles_item(span<const tile>(bg_bn_gfxTiles, 3), bpp_mode::BPP_4, compression_type::NONE), 
             bg_palette_item(span<const color>(bg_bn_gfxPal, 16), bpp_mode::BPP_4, compression_type::NONE),
             regular_bg_map_item(bg_bn_gfxMap[0], size(32, 32), compression_type::NONE, 1, false));
 }
